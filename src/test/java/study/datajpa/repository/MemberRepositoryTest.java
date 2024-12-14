ac64d5f28d7027243dda7ed11f788bcf569a947f
sjo <bonzoman@gmail.com>
2024-12-14 16:09:56 +0900

    hhgfkhjfg

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertEquals(findMember1, member1);
+        Assertions.assertEquals(findMember2, member2);
+
+        //리스트조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertEquals(all.size(), 2);
+
+        //카운트조회 검증
+        long count = memberRepository.count();
+        Assertions.assertEquals(count, 2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        Assertions.assertEquals(deletedCount, 0);
+    }
+
+
+}
